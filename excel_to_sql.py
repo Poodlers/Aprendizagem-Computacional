54061006556c9b188409bbe9986941f85363827f
Miguel Freitas <radiodomiguel70@gmail.com>
2022-09-22 17:10:11 +0100

    csv to SQL

---
+++ excel_to_sql.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import sqlite3
+
+acc_df = pd.read_csv('csvs/account.csv')
+card_dev_df = pd.read_csv('csvs/card_dev.csv')
+client_df = pd.read_csv('csvs/client.csv')
+disp_df = pd.read_csv('csvs/disp.csv')
+district_df = pd.read_csv('csvs/district.csv')
+loan_dev_df = pd.read_csv('csvs/loan_dev.csv')
+trans_dev_df = pd.read_csv('csvs/trans_dev.csv')
+
+conn = sqlite3.connect('bank_database.db')
+c = conn.cursor()
+
+acc_df.to_sql('account', con=conn, if_exists='replace', index=False)
+card_dev_df.to_sql('card_dev', con=conn, if_exists='replace', index=False)
+client_df.to_sql('client', con=conn, if_exists='replace', index=False)
+disp_df.to_sql('disp', con=conn, if_exists='replace', index=False)
+district_df.to_sql('district', con=conn, if_exists='replace', index=False)
+loan_dev_df.to_sql('loan_dev', con=conn, if_exists='replace', index=False)
+trans_dev_df.to_sql('trans_dev', con=conn, if_exists='replace', index=False)
+
+
+conn.commit()
+
+
+conn.close()
