4edc01418b146261ec42446551399821401db4b1
joaobcosta2001 <joaoassuncaoecosta@gmail.com>
2022-11-24 16:26:41 +0000

    updated tests.py to add new average_balance columns

--- tests.py
+++ tests.py
@@ -9,6 +9,8 @@ con = sqlite3.connect("bank_database.db")
 c = con.cursor()
 
 # Get table names
+
+'''
 res = con.execute("SELECT account_id,amount FROM trans_dev")
 amount = []
 account_id = []
@@ -32,3 +34,53 @@ for account in averageTransAmount:
     res = con.execute("UPDATE account SET average_trans_amount = " + str(averageTransAmount[account]) + " WHERE account_id = " + str(account))
     con.commit()
 
+'''
+
+
+
+'''
+res1 = con.execute("SELECT account_id FROM account")
+res1 = res1.fetchall()
+i=0
+nulls = 0
+not_nulls = 0
+average_balance = []
+for account in res1:
+    res2 = con.execute("SELECT amount FROM trans_dev WHERE account_id == " + str(account[0]))
+    res2 = res2.fetchall()
+    if len(res2) == 0:
+        res = con.execute("UPDATE account SET min_transaction_amount = NULL, max_transaction_amount = NULL WHERE account_id == " + str(account[0]))
+        nulls += 1
+    else:
+        not_nulls +=1
+        min = 99999999999999
+        max = 0
+        print(res2)
+        for balance in res2:
+            if balance[0] > max:
+                max = balance[0]
+            if balance[0] < min:
+                min = balance[0]
+        res = con.execute("UPDATE account SET min_transaction_amount = " + str(min) + ", max_transaction_amount = " + str(max) + " WHERE account_id == " + str(account[0]))
+    i+=1
+    print("{:.2f}".format(i/45) + "% (" + str(i) + "/4500)  " + str(not_nulls) + " not nulls and " + str(nulls) + " nulls",end="\r")
+con.commit()       
+
+'''
+
+res1 = con.execute("SELECT average_balance FROM account WHERE average_balance IS NOT NULL")
+res1 = res1.fetchall()
+print(len(res1))
+total = 0
+count = 0
+for balance in res1:
+    total += balance[0]
+    count += 1
+avg = total/count
+
+
+con.execute("UPDATE account SET average_balance = " + str(avg) + " WHERE average_balance IS NULL")
+
+con.commit()
+
+    
